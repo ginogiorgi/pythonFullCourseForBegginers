973816ed7032d7815f0ebfec03ed265d6cd7b846
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-07-05 20:49:03 -0300

    Numbers

---
+++ numbers_1.py
@@ -0,0 +1,22 @@
+import math
+
+x = 1
+y = 1.1
+z = 1 + 2j
+print(10 + 3)  # Suma
+print(10 - 3)  # Resta
+print(10 * 3)  # Multiplicación
+print(10 / 3)  # División
+print(10 // 3)  # División (entero)
+print(10 % 3)  # División (Resto)
+print(10**3)  # Elevado
+
+x = 10
+x = x + 3
+x += 3
+print(x)
+
+print(round(2.9))
+print(abs(-2.9))
+
+print(math.ceil(2.2))
