e93d0c597c6ec95da0b9e2d8af5fcff9504861c8
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-07-05 19:29:38 -0300

    String

---
+++ strings.py
@@ -0,0 +1,15 @@
+course = "Python Programming"
+message = """
+Hola buenos dias,
+
+Mucho texto.
+
+Saludos Gino.
+"""
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[2:])
+print(course[:5])
+print(course[:])
