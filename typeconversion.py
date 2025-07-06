bb03466748d47ac4d282076e0c858291cd48197b
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-07-05 21:00:48 -0300

    Type conversions

---
+++ typeconversion.py
@@ -0,0 +1,14 @@
+x = input("x: ")
+y = int(x) + 1
+print(f"x: {x}, y: {y}")
+
+
+# int(x) Converting to integer
+# float(x) Converting to float
+# bool(x) Converting to convert to boolean
+# str(x) Converting to string
+
+# Falsy
+# ""
+# 0
+# None
